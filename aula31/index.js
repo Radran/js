6fe7266d9c57a152eceef1360978193690b8010c
Felipe nery <147039864+Radran@users.noreply.github.com>
2023-11-17 05:55:56 -0300

    novo

---
+++ aula31/index.js
@@ -0,0 +1,14 @@
+/*const verdadeira = true
+let nome = 'Luiz'
+var nome2 = 'Luiz'
+
+var nome2 = 'Gustavo'
+if(verdadeira) {
+    let nome = 'Gustavo'
+    console.log(nome, nome2)
+}
+if(verdadeira) {
+    let nome = 'outra coisa'
+    console.log(nome, nome2)
+}*/
+
