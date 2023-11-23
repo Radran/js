fe5e91be581f5c03ca03e42699798286440651a5
Felipe nery <147039864+Radran@users.noreply.github.com>
2023-11-23 04:35:07 -0300

    aula nova

---
+++ aula32/index.js
@@ -0,0 +1,19 @@
+// ... rest, ...spread
+const nfc = [
+    ['49ers', 'Seahawks', 'Cardinals', 'Rams'],
+    ['Eagles', 'Cowboys', 'Commanders', 'Giants'],
+    ['Lions', 'Packers', 'Bears', 'vikings'],
+    ['Saints', 'Buccaneers', 'Panthers', 'Falcons']
+];
+[nfcWest, nfcEast, nfcNorth, nfcSouth] = nfc;
+console.log(nfcEast, nfcWest, nfcNorth, nfcSouth);
+const afc = [
+    ['Dolphins', 'Bills', 'Jets', 'Patriots'],
+    ['Chiefs', 'Broncos', 'Raiders', 'Chargers'],
+    ['Ravens', 'Browns', 'Steelers', 'Bengals'],
+    ['Jaguars', 'Texans', 'Colts', 'Titans']
+];
+[afcEast, afcWest, afcNorth, afcSouth] = afc;
+console.log(afcEast, afcWest, afcNorth, afcSouth);
+
+
