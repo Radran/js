edc991615caa3878b2a2ffb7fb4dc3892db6fd5a
Felipe nery <147039864+Radran@users.noreply.github.com>
2023-12-23 00:05:06 -0300

    novas aulas

---
+++ aula37/index.js
@@ -0,0 +1,20 @@
+const frutas = ['pera', 'maçã', 'uva']
+//for (let i = 0; i < frutas.length; i++) {
+  //  console.log(frutas[i]);
+/*const pessoa = {
+    nome: 'Felipe',
+    sobrenome:'Nery',
+    idade:19
+};
+for (let chave in pessoa) {
+    console.log(chave, pessoa[chave]);
+}*/
+const niners = {
+    qb: 'Purdy',
+    wr: 'Aiyuk',
+    te: 'Kittle',
+    rb: 'CMC'
+};
+for (let jogador in niners) {
+    console.log(`O ${jogador}, dos 49ers é ${niners[jogador]}`)
+}
---
+++ aula38/index.js
@@ -0,0 +1,16 @@
+//const nome = ['Felipe', 'Jonas', 'Ursula']
+//for (let i = 0; i < nome.length; i++) {
+ //   console.log(nome[i]);
+//}
+//for (let i in nome) {
+//    console.log(nome[i]);
+//}
+//for (let valor of nome) {
+  //  console.log(valor);
+//}
+//nome.forEach(function (valor, indice, array){
+//    console.log(valor, indice, array);
+//})
+//for classico - geralmente com iteráveis (array ou strings)
+//for in - retorna o indice ou chave (string, array ou objetos)
+// for of - retorna o valor em si (iteráveis, arrays ou strings)
