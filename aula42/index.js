089c0d7f6cf76afd7ee794bd0d1e0421dc1b1452
Felipe nery <147039864+Radran@users.noreply.github.com>
2024-01-03 05:58:37 -0300

    nova aula

---
+++ aula42/index.js
@@ -0,0 +1,11 @@
+function soma (x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        throw new Error('x e y precisam ser números');
+    }
+    return x + y;
+}
+try {
+    console.log(soma(1,'a'))
+}catch(error) {
+    console.log(error)
+}
