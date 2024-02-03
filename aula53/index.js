ef4458dc9578dd36729df74a9b8bccf4397aec89
Felipe nery <147039864+Radran@users.noreply.github.com>
2024-02-02 21:03:18 -0300

    NOVO

---
+++ aula53/index.js
@@ -0,0 +1,34 @@
+//factory function
+// constructor function
+function criapessoa(nome, sobrenome, altura, peso) {
+    return {
+        nome,
+        sobrenome,
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        },
+        //setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ')
+            this.nome = valor.shift()
+            this.sobrenome = valor.join(' ')
+        },
+        fala: function(assunto = 'falando sobre nada') {
+            return `${this.nome} está ${assunto}.`;
+        },
+        altura,
+        peso,
+        //getter
+        get imc() {
+            const indice = this.peso / (this.altura**2);
+            return indice.toFixed(2)
+        }
+    };
+}
+const p1 = criapessoa('Felipe','Nery', 1.78, 90)
+console.log(p1.nome)
+console.log(p1.sobrenome)
+console.log(p1.imc)
+p1.nomeCompleto='Lebron Raymone James'
+console.log(p1.nomeCompleto) 
+console.log(p1.fala())
