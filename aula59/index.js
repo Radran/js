05ffd4d679755a28b3e96ab73bf591e5225636c0
Felipe nery <147039864+Radran@users.noreply.github.com>
2024-03-08 10:14:15 -0300

    novo

---
+++ aula59/index.js
@@ -0,0 +1,9 @@
+//valor por referência
+//                  0         1        2        3          4
+//const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'rosana']
+//string, objetos, funçoes,numeros
+//const novo = nomes.slice(1, 3)
+//console.log(novo)
+const nomes = ['felipe', 'nery']
+const nome = nomes.join( ' ')
+console.log(nome)
