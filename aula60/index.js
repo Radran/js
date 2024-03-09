c2b2f9e091967a800a8a5d99319a774354a27f19
Felipe nery <147039864+Radran@users.noreply.github.com>
2024-03-09 06:12:29 -0300

    novo

---
+++ aula60/index.js
@@ -0,0 +1,12 @@
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia']
+//nomes.splice()
+//pop
+//nomes.splice(-1, 1)
+//shift
+//const removidos = nomes.splice(0, 1)
+//push
+//nomes.splice(nomes.length, 0, 'luiz')
+//unshift
+//nomes.splice(0, 0, 'Luiz')
+
+console.log(nomes,)
