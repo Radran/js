6dcd3adf9f5fadfdb028e679c5d5f72c20c32cbf
Felipe nery <147039864+Radran@users.noreply.github.com>
2024-06-12 15:02:19 -0300

    teste

--- sensi/index.js
+++ sensi/index.js
@@ -1,5 +1,8 @@
-let espaco = 30
-let dpi = 800
+let espaco = prompt('digite o espaço:');
+let dpi = prompt('digite seu dpi');
+espaco = Number(espaco);
+dpi = Number(dpi);
 const p1 = (-45 * espaco) + 3000 
 const sensi = (p1 / dpi) / 3.18 
-console.log((sensi))
+const res = sensi.toFixed(2)
+alert(`sua sensi ideal é ${res}`);
